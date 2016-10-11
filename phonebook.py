41efd09fa16adde66502588209b3e3a5f7749309
Kevin Farmer <farmekp0@sewanee.edu>
2016-10-11 08:46:08 -0400

    Committed.

--- phonebook.py
+++ phonebook.py
@@ -1,42 +1,50 @@
 import pickle
-
-
-print "Electronic Phone Book"
-print "====================="
-print "1\. Look up an entry"
-print "2\. Set an entry"
-print "3\. Delete an entry"
-print "4\. List all entries"
-print "5\. Quit"
-
-phonebook = {
-'Steve': '629-826-0134',
-'Britta': '217-264-1542',
-'Sarah': '629-983-0956',
-'Paul': '091-828-9070'
-}
+from os.path import exists
+if exists("phonebook.pickle"):
+        phonebook_file = open('phonebook.pickle', 'r')
+        phonebook = pickle.load (phonebook_file)
+        phonebook_file.close()
+else:
+    phonebook = {}
 
 while True:
-    numSelected = int(raw_input("What do you want to do (1-5)? "))
+    print "Electronic Phone Book"
+    print "====================="
+    print "1\. Look up an entry"
+    print "2\. Set an entry"
+    print "3\. Delete an entry"
+    print "4\. List all entries"
+    print "5\. Save entries"
+    print "6\. Quit"
+
+
+    numSelected = int(raw_input("What do you want to do (1-6)? "))
+
     if numSelected == 1:
         name = raw_input("Who would you like to look up? ")
-        def look_up(x):
-            print phonebook[x]
-        look_up(name)
+        if name in phonebook:
+            print phonebook[name]
+        else:
+            print "%s is not in the phonebook." % name
     elif numSelected == 2:
         nameGiven = raw_input("Who would you like to set? ")
         numGiven = raw_input("What is their phone number? ")
-        def set_entry(y, z):
-            phonebook[y] = [z]
-        set_entry(nameGiven, numGiven)
+        phonebook[nameGiven] = numGiven
+        print "Entry for %s made." % nameGiven
     elif numSelected == 3:
-        toDelete = raw_input("Who's entry should be deleted? ")
-        def delete_entry(a):
-            del phonebook[a]
-        delete_entry(toDelete)
+        toDelete = raw_input("Whose entry should be deleted? ")
+        del phonebook[toDelete]
+        print "Entry %s deleted." % toDelete
     elif numSelected == 4:
-        print phonebook.items()
+        for name, number in phonebook.items():
+            print "Found %s's number: %s" % (name, number)
     elif numSelected == 5:
-        print
-    else:
+        phonebook_file = open('phonebook.pickle', 'w')
+        pickle.dump(phonebook, phonebook_file)
+        phonebook_file.close()
+        print "Entries saved."
+    elif numSelected == 6:
+        print "Goodbye."
         break
+    else:
+        print "Invalid input."
